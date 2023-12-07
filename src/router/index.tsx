0ce072b3765ec37aa33fed56dcdf087fd7eb476d
Corentin Deleuse <dlse.corentin@gmail.com>
2023-12-07 17:19:54 +0100

    ⚠️ feat(error): Error page config

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,11 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../pages/Home";
 import Dashboard from "../pages/Dashboard";
+import ErrorPage from "../pages/ErrorPage";
 
 const router = createBrowserRouter([
     {
         path: "/",
-        element: <Home/>
+        element: <Home/>,
+        errorElement: <ErrorPage/>
     },
     {
         path: "/dashboard",
