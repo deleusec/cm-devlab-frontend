e21a762085930e37a6ea76ad4a85cd82e9061d0e
Corentin Deleuse <dlse.corentin@gmail.com>
2023-12-08 00:46:17 +0100

    ✒️ feat: Add poppins font

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,7 @@ export default {
         "secondary": "#1A1B41",
       },
       fontFamily: {
-        "sans": ["Poppins", "sans-serif"],
+        "poppins": ["Poppins", "sans-serif"],
       },
       minHeight: {
         "header" : "80px",
