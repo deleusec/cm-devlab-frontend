9e0ccc95e2ab69d53095b0bf59f39bb5a15c320c
Corentin Deleuse <dlse.corentin@gmail.com>
2023-12-11 03:48:05 +0100

    🔑 feat: update forgot password page

--- src/pages/authentication/ForgotPassword.tsx
+++ src/pages/authentication/ForgotPassword.tsx
@@ -1,7 +1,14 @@
+import FormButton from "@/components/form/FormButton";
+import FormInput from "@/components/form/FormInput";
+
 function ForgotPassword() {
     return ( 
-        <div>
-            <h1>ForgotPassword</h1>
+        <div className="w-full h-[100vh] bg-light-gray flex flex-col justify-center items-center gap-10">
+            <div className="flex flex-col gap-8 w-full max-w-[400px]">
+                <h1>Mot de passe oublié ?</h1>
+                <FormInput type="email" placeholder="email" />
+                <FormButton text="Se connecter" theme="dark" />      
+            </div>
         </div>
      );
 }
