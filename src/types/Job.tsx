249321601c8d0aa20d23eda25d6d4cdb5b526d1f
Corentin Deleuse <dlse.corentin@gmail.com>
2023-12-29 23:02:00 +0100

    feat: create jobs listing

--- src/types/Job.tsx
+++ src/types/Job.tsx
@@ -1,7 +1,15 @@
 export default interface Job {
-    id: number;
+    id: number;        
     name: string;
     description: string;
+    manual_handling: boolean;
+    awkward_postures: boolean;
+    hazardous_chemicals: boolean;
+    hyperbaric_environment: boolean;
+    extreme_temperatures: boolean;
+    noise: boolean;
     night_shift: boolean;
-    physical: boolean;
+    alternating_shifts: boolean;
+    repetitive_work: boolean;
+    job_score: number;
 }
