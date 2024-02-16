ec46256be383af19666d0e886fea5c46436df444
quentindrouet <quentin.drouet109@gmail.com>
2024-02-16 09:18:25 +0100

    Update colors

--- src/utils/colorScore.ts
+++ src/utils/colorScore.ts
@@ -1,9 +1,19 @@
 export default function colorScore(score: number, listScores: number[]): { backgroundColor: string, textColor: string } {
-    // get min and max score from listScores
+    if (listScores.length === 0) {
+        throw new Error("listScores is empty");
+    }
+
     const minScore = Math.min(...listScores);
     const maxScore = Math.max(...listScores);
 
-    // get list of 8 colors where in object text color is the background color but more darker and background color in object from min to max score (from green #9dc643 to red via yellow) 
+    if (minScore === maxScore) {
+        // Si tous les scores sont identiques, retournez simplement la première couleur
+        return {
+            backgroundColor: '#9dc643',
+            textColor: '#7e9e2d'
+        };
+    }
+
     const colors = [
         { backgroundColor: '#9dc643', textColor: '#7e9e2d' },
         { backgroundColor: '#c6d94e', textColor: '#a1b03b' },
@@ -15,9 +25,9 @@ export default function colorScore(score: number, listScores: number[]): { backg
         { backgroundColor: '#f03a2e', textColor: '#d01d2e' },
     ];
 
-    // get the index of the color in the list
-    const index = Math.round(((score - minScore) / (maxScore - minScore)) * (colors.length - 1));
+    let index = Math.round(((score - minScore) / (maxScore - minScore)) * (colors.length - 1));
+    // Assurer que l'index est dans les limites du tableau
+    index = Math.max(0, Math.min(index, colors.length - 1));
 
-    // return the color
     return colors[index];
 }
